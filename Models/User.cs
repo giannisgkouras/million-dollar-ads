9e94ee86930c01c36faa35c0a92dae981dc5c0ad
nikiforosper <56020828+nikiforosper@users.noreply.github.com>
2019-12-03 20:28:19 +0200

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads
+{
+    class User
+    {
+        public User()
+        {
+        }
+
+        public User(string username, string email, string password, string cpassword)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            CPassword = cpassword;
+        }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string CPassword { get; set; }
+
+        private static string error = "Κάτι πήγε στραβά.";
+
+        public static void ShowError()
+        {
+            System.Windows.Forms.MessageBox.Show(error);
+        }
+
+        public static bool IsEqual( User user1, User user2)
+        {
+            if (user1 == null || user2 == null) 
+            {
+                return false;
+            }
+            if(user1.Username != user2.Username)
+            {
+                error = "Ο χρήστης "+ user1.Username + " δεν υπάρχει!";
+                return false;
+            }
+            else if (user1.Password != user2.Password)
+            {
+                error = "Το όνομα χρήστη δεν ταιριάζει με τον κωδικό!";
+                return false;
+            }
+            return true;
+        }
+    }
+}
